19e107b0e4eb1599162465eabb07cdab0d97a8ae
Mohit Soni <sonimohit481.com>
2022-09-06 12:28:33 +0530

    First

---
+++ mmt/src/Components/Flight.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Flight = () => {
+  return <div>Flight</div>;
+};
---
+++ mmt/src/Components/Hotel.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Hotel = () => {
+  return <div>Hotel</div>;
+};
