e6a495c90351819b3c47eef4b9ae558c29612bda
Mohit Soni <sonimohit481.com>
2022-09-07 20:30:32 +0530

    new updates

--- mmt/src/App.js
+++ mmt/src/App.js
@@ -1,27 +1,22 @@
 import './App.css';
 import NavBar from './Components/NavBar';
-<<<<<<< HEAD
+
 import { Route, Routes } from "react-router-dom";
 import { Home } from './Components/Home/Home';
-=======
 import { Offers } from './Components/Offers';
-import {Location} from './Components/Location';
->>>>>>> 61e8cdff07b91f6fa3bd7246d2e1419cfe71e5cd
+import { Location } from './Components/Location';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <h1>Soni</h1>
-=======
-      <Offers/>
+      <Offers />
       <Location />
-      {/* <h1>Soni</h1> */}
->>>>>>> 61e8cdff07b91f6fa3bd7246d2e1419cfe71e5cd
     </div>
   );
 }
