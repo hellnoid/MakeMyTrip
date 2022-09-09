4cb1dceb1fc1161bed6b830d6bbf3c1dd97e4b7a
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-09-09 22:21:15 +0530

    allChange

--- mmt/src/Functions/auth/auth.constants.js
+++
@@ -1,9 +0,0 @@
-import { getValue } from "../../Utils/LocalStorage"
-
-export const initialState = {
-    isUserLoggedIn: getValue("userToken") ? true : false,
-    userToken: getValue("userToken"),
-    userName: getValue("userName"),
-    isLoading: false,
-    error: false
-}
